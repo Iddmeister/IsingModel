9f849a02bd567050dbffcb403cb305dff3818b88
Iddmeister <iddonagenda.t@gmail.com>
2025-03-21 16:22:03 +0000

    Rewrote main.cpp to use Config objects and run multiple configurations

--- main.cpp
+++ main.cpp
@@ -1,29 +1,6 @@
-// #include <random>
-#include <cstdlib>
 #include <iostream>
-#include <cmath>
+#include "config.cpp"
 
-double calculate_energy(int selection, int atoms[], double J) {
-
-    double energy_change;
-
-    if (selection == 0) {
-
-        energy_change = -J*atoms[selection]*atoms[selection+1];
-
-    } else if (selection == 100-1) {
-
-        energy_change = -J*atoms[selection]*atoms[selection-1];
-
-
-    } else {
-
-        energy_change = (-J*atoms[selection]*atoms[selection+1]) + (-J*atoms[selection]*atoms[selection-1]);
-
-    }
-
-    return energy_change;
-}
 
 int main() {
 
@@ -31,58 +8,26 @@ int main() {
 
     srand(seed);
 
-    double J  = 1;
-    double temperature = 298;
-    double beta = 1/(1.381e-23*temperature);
-
     int num_atoms = 100;
-
-    int atoms[num_atoms];
-
-    for (int i = 0; i < num_atoms; ++i) {
-
-        int state = rand()%2 == 0 ? 1 : -1;
-
-        atoms[i] = state;
-
-    // std::cout << atoms[i] << "\n";
-
-    }
-
+    double J = 1;
+    double temperature = 298;
+    
+    int numConfigs = 100;
     int iterations = 20000;
 
-    for (int i = 0; i < iterations; ++i) {
+    std::vector<Config> configurations;
 
-        int selection = rand()%(num_atoms+1);
+    for (int i = 0; i < numConfigs; ++i) {
 
-        double energy_change = calculate_energy(selection, atoms, J);
-        
-        double probability = exp(-beta*energy_change);
-        // std::cout << energy_change;
-
-        if (probability > 1) probability = 1;
-
-        if ((rand()%100000)/1e5 < probability) {
-
-            atoms[selection] *= -1;
-        
-        }
+        Config new_config = Config(num_atoms, J, temperature);
+        new_config.simulate(iterations);
+        configurations.push_back(new_config);
+        std::cout << "\nConfiguration " << i+1 << "\nTotal Energy: " << new_config.calculateEnergy() << "\nMagnetisation: " << new_config.calculateMagnetism() << "\n";
 
     }
-
-    double total_energy = 0;
-    int magnetisation = 0;
-
-    for (int i = 0; i < num_atoms; ++i) {
-        total_energy += calculate_energy(i, atoms, J);
-        magnetisation += atoms[i];
-    }
-
-    std::cout << "\n total energy: " << total_energy << " \n mag: " << magnetisation << "\n";
 
     return 0;
 
-
-    // std::mt19937 gen = std::mt19937(seed);
-
 }
+
+
