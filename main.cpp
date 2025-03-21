21c774249a4996ad6e1886440cd2704c9c88abfc
Iddmeister <iddonagenda.t@gmail.com>
2025-03-21 16:28:27 +0000

    Each configs energy and magnetism are output to a file

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include "config.cpp"
-
+#include <fstream>
 
 int main() {
+
+    std::ofstream output;
 
     int seed = 123500;
 
@@ -17,14 +19,23 @@ int main() {
 
     std::vector<Config> configurations;
 
+    output.open("output.txt");
+
+    output << "config,energy,magnetism" << std::endl;
+
     for (int i = 0; i < numConfigs; ++i) {
 
         Config new_config = Config(num_atoms, J, temperature);
         new_config.simulate(iterations);
         configurations.push_back(new_config);
-        std::cout << "\nConfiguration " << i+1 << "\nTotal Energy: " << new_config.calculateEnergy() << "\nMagnetisation: " << new_config.calculateMagnetism() << "\n";
+
+        output << i << "," << new_config.calculateEnergy() << "," << new_config.calculateMagnetism() << std::endl;
+
+        // std::cout << "\nConfiguration " << i+1 << "\nTotal Energy: " << new_config.calculateEnergy() << "\nMagnetisation: " << new_config.calculateMagnetism() << "\n";
 
     }
+
+    output.close();
 
     return 0;
 
