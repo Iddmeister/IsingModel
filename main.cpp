4b81730c960570a9dafa41be95e0368ec6a70fd4
Iddmeister <iddonagenda.t@gmail.com>
2025-03-24 16:32:16 +0000

    main.cpp runs for multiple test iterations, the resuts are plotted using python

--- main.cpp
+++ main.cpp
@@ -2,83 +2,111 @@
 #include <cstdlib>
 #include <iostream>
 #include <cmath>
+#include <fstream>
+#include <vector>
 
 double calculate_energy(int selection, int atoms[], double J) {
 
-    double energy_change;
+    double energy;
 
     if (selection == 0) {
 
-        energy_change = -J*atoms[selection]*atoms[selection+1];
+        energy = -J*atoms[selection]*atoms[selection+1];
 
     } else if (selection == 100-1) {
 
-        energy_change = -J*atoms[selection]*atoms[selection-1];
+        energy = -J*atoms[selection]*atoms[selection-1];
 
 
     } else {
 
-        energy_change = (-J*atoms[selection]*atoms[selection+1]) + (-J*atoms[selection]*atoms[selection-1]);
+        energy = (-J*atoms[selection]*atoms[selection+1]) + (-J*atoms[selection]*atoms[selection-1]);
 
     }
 
-    return energy_change;
+    return energy;
 }
 
 int main() {
 
     int seed = 1234;
 
-    srand(seed);
 
     double J  = 1;
     double temperature = 298;
     double beta = 1/(1.381e-23*temperature);
 
-    int num_atoms = 100;
+    std::vector<int> possible_iterations;
 
-    int atoms[num_atoms];
+    for (int i = 1; i < 8; ++i) {
 
-    for (int i = 0; i < num_atoms; ++i) {
-
-        int state = rand()%2 == 0 ? 1 : -1;
-
-        atoms[i] = state;
-
-    // std::cout << atoms[i] << "\n";
+        possible_iterations.push_back(1*pow(10, i));
 
     }
 
-    int iterations = 200000;
 
-    for (int i = 0; i < iterations; ++i) {
+    std::ofstream output;
 
-        int selection = rand()%(num_atoms+1);
+    output.open("output.txt");
 
-        double energy_change = calculate_energy(selection, atoms, J);
+    output << "iterations,energy,magnetisation" << std::endl;
+
+    for (int p = 0; p < possible_iterations.size(); ++p) {
         
-        double probability = exp(-beta*energy_change);
-        // std::cout << energy_change;
+        srand(seed);
 
-        if (probability > 1) probability = 1;
+        int num_atoms = 100;
 
-        if ((rand()%100000)/1e5 < probability) {
+        int atoms[num_atoms];
 
-            atoms[selection] *= -1;
-        
+        for (int i = 0; i < num_atoms; ++i) {
+
+            int state = rand()%2 == 0 ? 1 : -1;
+
+            atoms[i] = state;
+
+        // std::cout << atoms[i] << "\n";
+
         }
 
+
+        int iterations = possible_iterations[p];
+
+        for (int i = 0; i < iterations; ++i) {
+
+            int selection = rand()%(num_atoms+1);
+
+            double energy_change = calculate_energy(selection, atoms, J)*2;
+            
+            double probability = exp(-beta*energy_change);
+            // std::cout << energy_change;
+
+            if (probability > 1) probability = 1;
+
+            if ((rand()%100000)/1e5 < probability) {
+
+                atoms[selection] *= -1;
+            
+            }
+
+        }
+
+        double total_energy = 0;
+        int magnetisation = 0;
+
+        for (int i = 0; i < num_atoms; ++i) {
+            total_energy += calculate_energy(i, atoms, J);
+            magnetisation += atoms[i];
+        }
+
+        std::cout << "\n iterations: " << iterations << "\n total energy: " << total_energy << " \n mag: " << magnetisation << "\n";
+
+        output << iterations << "," << total_energy << "," << magnetisation << std::endl;
+
     }
 
-    double total_energy = 0;
-    int magnetisation = 0;
+    output.close();
 
-    for (int i = 0; i < num_atoms; ++i) {
-        total_energy += calculate_energy(i, atoms, J);
-        magnetisation += atoms[i];
-    }
-
-    std::cout << "\n total energy: " << total_energy << " \n mag: " << magnetisation << "\n";
 
     return 0;
 
