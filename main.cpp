8124355e425bf6ebde05d4b9f056d2cfdaaae175
Iddmeister <iddonagenda.t@gmail.com>
2025-03-25 21:04:58 +0000

    Commented Code

--- main.cpp
+++ main.cpp
@@ -1,24 +1,25 @@
-// #include <random>
 #include <cstdlib>
 #include <iostream>
 #include <cmath>
 
-double calculate_energy(int selection, int atoms[], double J) {
+//This is the original algorithm for generating a configuration using the Ising model
+
+double calculateEnergy(int selection, int atoms[], double J, int flip=1) {
 
     double energy_change;
 
     if (selection == 0) {
 
-        energy_change = -J*atoms[selection]*atoms[selection+1];
+        energy_change = -J*atoms[selection]*flip*atoms[selection+1];
 
     } else if (selection == 100-1) {
 
-        energy_change = -J*atoms[selection]*atoms[selection-1];
+        energy_change = -J*atoms[selection]*flip*atoms[selection-1];
 
 
     } else {
 
-        energy_change = (-J*atoms[selection]*atoms[selection+1]) + (-J*atoms[selection]*atoms[selection-1]);
+        energy_change = (-J*atoms[selection]*flip*atoms[selection+1]) + (-J*atoms[selection]*atoms[selection-1]);
 
     }
 
@@ -27,41 +28,47 @@ double calculate_energy(int selection, int atoms[], double J) {
 
 int main() {
 
+    //The randomness is seeded so that results can be repeated
     int seed = 1234;
-
     srand(seed);
 
+    //Constants required for the algorithm are defined here
     double J  = 1;
     double temperature = 298;
     double beta = 1/(1.381e-23*temperature);
 
+    //The number of atoms to simulate the spin of
     int num_atoms = 100;
 
     int atoms[num_atoms];
 
     for (int i = 0; i < num_atoms; ++i) {
-
+        
+        //All of the atoms are assigned a spin of -1 or +1 randomly
         int state = rand()%2 == 0 ? 1 : -1;
 
         atoms[i] = state;
 
-    // std::cout << atoms[i] << "\n";
-
     }
 
+    //The number of iterations to simulate for
     int iterations = 200000;
 
     for (int i = 0; i < iterations; ++i) {
 
+        //A random atom is selected each iteration
         int selection = rand()%(num_atoms+1);
 
-        double energy_change = calculate_energy(selection, atoms, J);
+        //The energy change of flipping the atom's spin is calculated using the calculateEnergy function
+        double energy_change = calculateEnergy(selection, atoms, J) - calculateEnergy(selection, atoms, J, -1);
         
+        //The probability of this change is calculated using the partition function
         double probability = exp(-beta*energy_change);
-        // std::cout << energy_change;
 
+        //The probability is capped at 1
         if (probability > 1) probability = 1;
 
+        //The probability is compared to a random number between 0 and 1, and the atom's spin is flipped depending on this
         if ((rand()%100000)/1e5 < probability) {
 
             atoms[selection] *= -1;
@@ -74,15 +81,16 @@ int main() {
     int magnetisation = 0;
 
     for (int i = 0; i < num_atoms; ++i) {
-        total_energy += calculate_energy(i, atoms, J);
+
+        //The total energy and magnetism are summed over every atom
+        total_energy += calculateEnergy(i, atoms, J);
         magnetisation += atoms[i];
     }
 
-    std::cout << "\n total energy: " << total_energy << " \n mag: " << magnetisation << "\n";
+    //The final values are output
+    std::cout << "\n total energy: " << total_energy << " \n mag: " << magnetisation << "\n\n";
 
     return 0;
 
-
-    // std::mt19937 gen = std::mt19937(seed);
 
 }
