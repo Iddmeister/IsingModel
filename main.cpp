6c33aa3017adce0d0bb6f5f3c5863143a022fba2
Iddmeister <iddonagenda.t@gmail.com>
2025-03-31 05:13:12 +0100

    Added output directory and temperaures to command line arguments

--- main.cpp
+++ main.cpp
@@ -14,13 +14,29 @@ int main(int argc, char* argv[]) {
     double J = 1;
     int seed = 12345678;
 
-    std::vector<double> temperatures = {1e20, 2.5e22, 5e22, 7.5e22, 1e23, 2.5e23, 5e23, 7.5e23, 1e25, 1e27};
+    std::string outputDirectory;
 
+    std::vector<double> temperatures;
+
+    //Pulling arguments from the command line with default values and error messages for incorrect inputs
     try {numAtoms = argc > 1 ? std::stoi(argv[1]) : 100;} catch (...) {std::cout << "\nError in number of atoms input\n\n"; return 0;};
     try {numConfigs = argc > 2 ? std::stoi(argv[2]) : 1000;} catch (...) {std::cout << "\nError in number of configurations input\n\n"; return 0;};
     try {iterations = argc > 3 ? std::stoi(argv[3]) : 1e5;} catch (...) {std::cout << "\nError in number of iterations input\n\n"; return 0;};
     try {J = argc > 4 ? std::stof(argv[4]) : 1;} catch (...) {std::cout << "\nError in J input\n\n"; return 0;};
     try {seed = argc > 5 ? std::stoi(argv[5]) : 12345678;} catch (...) {std::cout << "\nError in seed input\n\n"; return 0;};
+    try {outputDirectory = argc > 6 ? argv[6] : "output_test";} catch (...) {std::cout << "\nError in output directory input\n\n"; return 0;};
+
+    if (argc == 7) {
+        temperatures = {1e20, 2.5e22, 5e22, 7.5e22, 1e23, 2.5e23, 5e23, 7.5e23, 1e25, 1e27};
+    }
+
+    for (int t = 7; t < argc; t++) {
+
+        double new_temp;
+        try {new_temp = std::stof(argv[t]);} catch (...) {std::cout << "\nError in temperature input\n\n"; return 0;};
+        temperatures.push_back(new_temp);
+
+    }
 
     for (int t = 0; t < temperatures.size(); ++t) {
 
@@ -30,7 +46,7 @@ int main(int argc, char* argv[]) {
 
         container.simulate(iterations);
         std::stringstream filename;
-        filename << "output/temperature_" << t << ".csv";
+        filename << outputDirectory << "/temperature_" << t << ".csv";
         container.outputResults(filename.str());
 
     }
